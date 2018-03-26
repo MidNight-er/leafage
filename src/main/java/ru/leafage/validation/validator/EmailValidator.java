7904a5dffc20ae364e04519f5cd59aff2b3df26c
midnighter <midnighterdev@gmail.com>
2018-03-26 22:21:36 +0300

    add email tld validation

---
+++ src/main/java/ru/leafage/validation/validator/EmailValidator.java
@@ -0,0 +1,33 @@
+package ru.leafage.validation.validator;
+
+import ru.leafage.validation.constraints.EmailWithTld;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements ConstraintValidator<EmailWithTld, String> {
+
+    private Pattern pattern;
+
+    private Matcher matcher;
+
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$";
+
+    @Override
+    public void initialize(EmailWithTld constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return (validateEmail(email));
+    }
+
+    private boolean validateEmail(String email) {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
